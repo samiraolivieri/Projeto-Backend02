2905052d8b920a05a6eb91d887b46eca15c1297c
Samira Olivieri <art0066441@aluno.firjansenaisesi.com.br>
2026-03-12 13:47:53 -0300

    Segundo Commit do Servidor

---
+++ controller/controller.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express.Router()
+app.use(express.json)
+//rotas
+app.get("/usuario", () => {})
+
+app.get("/usuario/:id", () => {})
+
+app.post("/usuario", () => {})
+
+app.put("/usuario/:id", () => {})
+
+app.delete("/usuario/:id", () => {})
+
+module.exports = app
